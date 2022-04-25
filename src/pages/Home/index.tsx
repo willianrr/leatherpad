e3185b5e79fc71c93d25225d6c33d7dbd45acdc7
Willian  Regina <willianrogerioregina@gmail.com>
2022-04-25 20:26:18 -0300

    ajustes pasta da home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const HomePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div>teste</div>
+    </>
+  )
+}
+
+export default HomePage
